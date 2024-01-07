cd50fa0ed60b063e0abfc3a5b38da802109b50d2
bigchiano <bigchiano@protonmail.com>
2024-01-07 19:41:47 +0200

    update entrypoint

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,23 +1,15 @@
 use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    msg,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey
 };
 
-// declare and export the program's entrypoint
+use crate::processor::Processor;
+
 entrypoint!(process_instruction);
 
-// program entrypoint's implementation
 fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    _instruction_data: &[u8]
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    // log a message to the blockchain
-    msg!("Hello, world!");
-
-    // gracefully exit the program
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
