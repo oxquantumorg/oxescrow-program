4e3e711d0973f94a295a6d06a31a4dd798acd88e
bigchiano <bigchiano@protonmail.com>
2024-01-24 22:11:35 +0200

    format file

--- program/src/instructions/wallet_escrow/register_escrow.rs
+++ program/src/instructions/wallet_escrow/register_escrow.rs
@@ -1,14 +1,6 @@
-use solana_program::{
-    account_info::AccountInfo,
-    msg,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-};
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey};
 
-pub fn handler(
-    _accounts: &[AccountInfo],
-    _program_id: &Pubkey,
-) -> ProgramResult {
+pub fn handler(_accounts: &[AccountInfo], _program_id: &Pubkey) -> ProgramResult {
     msg!("Register Escrow!");
     Ok(())
 }
