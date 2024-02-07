c8b037f1ff2e514cf1cd3ba2dcfefce3c66ed790
bigchiano <bigchiano@protonmail.com>
2024-02-07 18:15:37 +0200

    change expiry date

--- program/src/utils/constants.rs
+++ program/src/utils/constants.rs
@@ -2,5 +2,5 @@
 pub const DEFAULT_ESCROW_STATE_LEN: usize = 113;
 pub const ESCROW_REGISTRY_STATE_LEN: usize = 65;
 pub const ESCROW_SEED: &[u8] = b"escrow";
-pub const ESCROW_WALLET_SEED: &[u8] = b"escrow_w";
-pub const ESCROW_WAIT_TIME_SEC: i64 = 2;
+pub const ESCROW_WALLET_SEED: &[u8] = b"escrow ";
+pub const ESCROW_WAIT_TIME_SEC: i64 = 300;
